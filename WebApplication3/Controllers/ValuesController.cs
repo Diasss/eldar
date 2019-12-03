8a53b9c66785e22fbc6cb53d300715d06d27485e
Оспанов Диас <osp.dasik@mail.ru>
2019-12-03 16:07:55 +0600

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : Controller
+    {
+        Model1 db = new Model1();
+        // GET: Values
+        public IEnumerable<Products> GetProducts()
+        {
+            return db.Products;
+        }
+        public Products GetProducts(int id)
+        {
+            Products pr = db.Products.Find(id);
+            return pr;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication3/Models/InitialDatas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class InititalData : DropCreateDatabaseAlways<Model1>
+    {
+        protected override void Seed(Model1 context)
+        {
+
+            context.Products.Add(new Products { Name = "Pomidor", Cost = 200 });
+            context.Products.Add(new Products { Name = "Coconut", Cost = 600 });
+            context.Products.Add(new Products { Name = "Cucumber", Cost = 100 });
+
+        }
+
+    }
+
+}
